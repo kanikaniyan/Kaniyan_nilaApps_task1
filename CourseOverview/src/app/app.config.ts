40430c1632f8f1271778cf9b3db75c815b4b6aac
kanikaniyan <kkaniyan96@gmail.com>
2024-09-29 18:49:14 +0530

    updated the UI functions

--- CourseOverview/src/app/app.config.ts
+++ CourseOverview/src/app/app.config.ts
@@ -6,8 +6,9 @@ import { provideHttpClient } from '@angular/common/http';
 
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    BrowserModule, provideHttpClient(), provideCharts(withDefaultRegisterables())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations(),
+    BrowserModule, BrowserAnimationsModule, provideHttpClient(), provideCharts(withDefaultRegisterables())]
 };
--- CourseOverview/src/app/components/programs/programs.component.ts
+++ CourseOverview/src/app/components/programs/programs.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CourseService } from '../../services/course.service';
 import { CommonModule } from '@angular/common';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { AssessmentProgressComponent } from '../assessment-progress/assessment-progress.component';
 import { AttendanceReportComponent } from '../attendance-report/attendance-report.component';
 import { OverallCourseOutcomeAttainmentComponent } from '../overall-course-outcome-attainment/overall-course-outcome-attainment.component';
@@ -8,15 +9,38 @@ import { CourseOutcomeAttainmentComponent } from '../course-outcome-attainment/c
 import { EvaluationStatusComponent } from '../evaluation-status/evaluation-status.component';
 import { SkeletonComponent } from '../skeleton/skeleton.component';
 import { Course } from '../../model';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-programs',
   standalone: true,
-  imports: [CommonModule, AssessmentProgressComponent, AttendanceReportComponent, EvaluationStatusComponent, 
-    CourseOutcomeAttainmentComponent, OverallCourseOutcomeAttainmentComponent, SkeletonComponent],
+  imports: [CommonModule, AssessmentProgressComponent, AttendanceReportComponent, EvaluationStatusComponent,
+    CourseOutcomeAttainmentComponent, OverallCourseOutcomeAttainmentComponent, SkeletonComponent,
+  ],
   templateUrl: './programs.component.html',
-  styleUrl: './programs.component.css'
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({
+        height: '0',
+        opacity: '0',
+      })),
+      state('expanded', style({
+        height: '*',
+        opacity: '1',
+      })),
+      transition('expanded <=> collapsed', [
+        animate('300ms ease-in-out')
+      ]),
+    ]),
+    trigger('rotateIcon', [
+      state('down', style({ transform: 'rotate(0deg)' })),
+      state('up', style({ transform: 'rotate(180deg)' })),
+      transition('up <=> down', animate('300ms ease-in-out')),
+    ]),
+  ],
+  styleUrl: './programs.component.css',
 })
+
 export class ProgramsComponent implements OnInit {
   courseData: Course | null = null;
 
@@ -24,24 +48,50 @@ export class ProgramsComponent implements OnInit {
   isLoading: boolean = true;
   isLoading1: boolean = true;
 
-  constructor(private courseService: CourseService) {}
+  constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
     // Fetch course data on component initialization
-    this.courseService.getCourseData().subscribe(data => {
-      console.log(data);
-      this.courseData = data.courses[0];
+    this.courseService.getCourseData()
+      .pipe(delay(1000))
+      .subscribe(data => {
+        console.log(data);
+        this.courseData = data.courses[0];
 
-        setTimeout(() => {
-          this.isLoading = false;
-          this.isLoading1 = false;
-        }, 1000);
-    });
+        this.isLoading = false;
+        this.isLoading1 = false;
+
+      });
+
+    this.checkScreenSize();
+  }
+
+  isExpanded = false;
+  isMobile = false;
+  isTabView = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isTabView = window.innerWidth < 1024 && window.innerWidth >= 768; 
+    this.isMobile = window.innerWidth < 1024;
+    if (!this.isMobile) {
+      this.isExpanded = true;
+    }
+  }
+
+  toggleExpand() {
+    if (this.isMobile) {
+      this.isExpanded = !this.isExpanded;
+    }
   }
 
   toggleObeReport() {
-      this.isLoading1 = true;
-      setTimeout(() => {
+    this.isLoading1 = true;
+    setTimeout(() => {
       this.isObeReport = !this.isObeReport;
       this.isLoading1 = false;
     }, 1000);
